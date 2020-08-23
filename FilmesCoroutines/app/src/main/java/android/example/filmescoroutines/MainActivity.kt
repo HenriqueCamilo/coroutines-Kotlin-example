f30d23b26c0b894d18120bf3fef47ad841c061b8
Henrique <henriquecamilomapa@gmail.com>
2020-08-23 19:22:38 -0300

    coroutines

---
+++ FilmesCoroutines/app/src/main/java/android/example/filmescoroutines/MainActivity.kt
@@ -0,0 +1,18 @@
+package android.example.filmescoroutines
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.example.filmescoroutines.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                    .replace(R.id.container, MainFragment.newInstance())
+                    .commitNow()
+        }
+    }
+}
---
+++ FilmesCoroutines/app/src/main/java/android/example/filmescoroutines/ui/main/Filme.kt
@@ -0,0 +1,6 @@
+package android.example.filmescoroutines.ui.main
+
+data class Filme(
+    val id:Int,
+    val titulo: String
+)
